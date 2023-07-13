5895ff77cd4a32c2ac5551bf1e712306afe80195
Wajid <ansariwajid9999@gmail.com>
2023-07-13 18:52:00 +0530

    encapsulation implemneted done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
-public class Main {
-  
+public class Main
+{
+    public static void main(String[] args)
+    {
+        RWOnly rw=new RWOnly();
+        //rw.name="name";
+        //java: name has private access in com.driver.RWOnly
+
+        //System.out.print(rw.name);
+        //java: name has private access in com.driver.RWOnly
+
+        rw.setName("name");
+        System.out.println(rw.getName());
+    }
 }
